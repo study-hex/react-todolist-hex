ce77eeb36db8fe877ec2a45903e1db93b4b899cc
Aya <90342033+Aya-X@users.noreply.github.com>
2023-09-05 17:55:22 +0900

    style: Add base setting
    - CDN
    - font
    - color

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,15 @@
 module.exports = {
-  content: ['index.html','./src/**/*.{js,jsx,ts,tsx,vue,html}'],
+  content: ['index.html', './src/**/*.{js,jsx,ts,tsx,vue,html}'],
   theme: {
+    colors: {
+      primary: '#FFD370',
+      dark: '#333333',
+      light: '#9F9A91',
+    },
+    fontFamily: {
+      noto: ['Noto Sans TC', 'sans-serif'],
+    },
     extend: {},
   },
   plugins: [],
-}
+};
