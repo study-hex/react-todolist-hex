3b501630c83e7034e6f2c263ad56c3765cea5eb3
Aya <90342033+Aya-X@users.noreply.github.com>
2023-09-05 16:37:30 +0900

    feat: Add hashRouter and routes

---
+++ src/routes/Signup.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Signup(): React.ReactElement {
+  return (
+    <div className="text-center selection:bg-green-900">
+      <h2>Signup</h2>
+    </div>
+  );
+}
+
+export default Signup;
---
+++ src/routes/Todo.tsx
@@ -0,0 +1,22 @@
+import React, { useState } from 'react';
+
+function Todo(): React.ReactElement {
+  const [count, setCount] = useState<number>(0);
+
+  return (
+    <div className="text-center selection:bg-green-900">
+      <header className="flex min-h-screen flex-col items-center justify-center bg-[#282c34] text-white">
+        <button
+          type="button"
+          className="my-6 rounded bg-gray-300 px-2 py-2 text-[#282C34] transition-all hover:bg-gray-200"
+          onClick={() => setCount((prevCount) => prevCount + 1)}
+        >
+          count is:
+          <span className="pl-1">{count}</span>
+        </button>
+      </header>
+    </div>
+  );
+}
+
+export default Todo;
