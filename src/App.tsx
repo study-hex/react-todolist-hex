b2ee8c2db7cfb0df237c7200615fc3cad86ad29f
Aya <90342033+Aya-X@users.noreply.github.com>
2023-09-08 13:58:29 +0800

    style: Add RWD

--- src/App.tsx
+++ src/App.tsx
@@ -15,8 +15,8 @@ import ImgHero from './images/hero.webp';
 
 function PublicLayout() {
   return (
-    <div className="container min-h-screen px-8 py-12 md:grid md:grid-cols-12 md:items-center md:gap-[103px]">
-      <header className="md:col-span-5 md:col-start-2">
+    <div className="container mx-auto min-h-screen px-8 py-12 sm:flex sm:flex-wrap sm:items-center sm:justify-between lg:grid lg:grid-cols-12 lg:gap-[103px] xl:gap-[120px]">
+      <header className="sm:w-full md:mx-auto md:w-[47%] lg:col-span-5 lg:col-start-2 lg:w-full">
         <Link
           to="/signup"
           className="leading-[3rem] hover:opacity-80"
@@ -27,16 +27,16 @@ function PublicLayout() {
           </h1>
         </Link>
 
-        <figure className="hidden w-full md:inline-block">
+        <figure className="hidden w-full lg:inline-block">
           <img
             src={ImgHero}
             alt="hero"
-            className="mx-auto object-cover md:block md:aspect-square"
+            className="mx-auto object-cover lg:block lg:aspect-square"
           />
         </figure>
       </header>
 
-      <main className="md:col-span-4">
+      <main className="sm:w-full md:mx-auto md:w-3/5 lg:col-span-4 lg:w-full">
         <Outlet />
       </main>
     </div>
