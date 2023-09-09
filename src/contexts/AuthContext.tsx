ebac0a848796f35512a409d676ee3052dd389291
Aya <90342033+Aya-X@users.noreply.github.com>
2023-09-09 15:08:34 +0800

    feat: Add remove and move save to AuthProvider

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -3,23 +3,39 @@ import { useState, useMemo, useEffect, useContext, createContext } from 'react';
 interface IAuthContextData {
   token: string;
   setToken: (token: string) => void;
+  saveToken: (resToken: string) => void;
+  removeToken: () => void;
 }
 
 interface IAuthProviderProps {
   children: React.ReactNode;
 }
+// end of interface
 
 const AuthContext = createContext<IAuthContextData | undefined>(undefined);
 
 export function AuthProvider({ children }: IAuthProviderProps) {
-  const [token, setToken] = useState('');
+  const [token, setToken] = useState<string>('');
 
-  const authContextData: IAuthContextData = useMemo(() => {
-    return {
-      token,
-      setToken,
-    };
-  }, [token, setToken]);
+  const saveToken = (resToken: string) => {
+    // console.log('AUTH_TOKEN:::', resToken);
+    setToken(resToken);
+    // localStorage.setItem('AUTH_TOKEN', resToken);
+
+    const tomorrow = new Date();
+    tomorrow.setDate(tomorrow.getDate() + 1);
+    tomorrow.setHours(1, 0, 0, 0);
+
+    document.cookie = `hexschoolTodo=${resToken}; expires=${tomorrow.toUTCString()}`;
+  };
+  // end of saveToken(resToken)
+
+  function removeToken() {
+    document.cookie =
+      'hexschoolTodo=; expires=Thu, 01 Jan 1970 00:00:00 GMT; path=/';
+    setToken('');
+  }
+  // end of removeToken
 
   useEffect(() => {
     const cookieValue = document.cookie
@@ -31,6 +47,17 @@ export function AuthProvider({ children }: IAuthProviderProps) {
       setToken(cookieValue);
     }
   }, []);
+  // end of useEffect()
+
+  const authContextData: IAuthContextData = useMemo(() => {
+    return {
+      token,
+      setToken,
+      saveToken,
+      removeToken,
+    };
+  }, [token, setToken]);
+  // end of useMemo()
 
   return (
     <AuthContext.Provider value={authContextData}>
@@ -38,6 +65,7 @@ export function AuthProvider({ children }: IAuthProviderProps) {
     </AuthContext.Provider>
   );
 }
+// end of AuthProvider()
 
 export function useAuth() {
   const authContextData = useContext(AuthContext);
@@ -48,3 +76,4 @@ export function useAuth() {
 
   return authContextData;
 }
+// end of useAuth()
