6d79518fde9cc1ea8b29c4636920e4ae56a335cf
Aya <90342033+Aya-X@users.noreply.github.com>
2023-09-10 22:23:47 +0800

    feat: Update form style to react-hook-form

--- src/routes/Signup.tsx
+++ src/routes/Signup.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 
-import { Formik, Form, Field } from 'formik';
+import { useForm } from 'react-hook-form';
 import * as Yup from 'yup';
+import { yupResolver } from '@hookform/resolvers/yup';
 
 import { api } from '../helpers/api';
 import { Toast } from '../components/Toast';
@@ -20,142 +21,154 @@ const SignupSchema = Yup.object().shape({
     .oneOf([Yup.ref('password')], 'Passwords does not match')
     .required('Required'),
 });
+// end of SignupSchema
 
 function Signup(): React.ReactElement {
   const navigate = useNavigate();
 
-  return (
-    <Formik
-      initialValues={{
-        nickname: '',
-        email: '',
-        password: '',
-        confirmPassword: '',
-      }}
-      validationSchema={SignupSchema}
-      onSubmit={(values, { resetForm }) => {
-        // setTimeout(() => {
-        //   alert(JSON.stringify(values, null, 2));
-        // }, 400);
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+    reset,
+  } = useForm({
+    resolver: yupResolver(SignupSchema),
+    defaultValues: {
+      nickname: '',
+      email: '',
+      password: '',
+      confirmPassword: '',
+    },
+  });
 
-        const data = {
-          email: values.email,
-          password: values.password,
-          nickname: values.nickname,
-        };
+  const onSubmitHandler = (data: any) => {
+    console.log({ data });
+    // console.log(watch('email'));
+    const sendData = {
+      email: data.email,
+      password: data.password,
+      nickname: data.nickname,
+    };
 
-        api.signup(data).then((res: any) => {
-          if (!res?.status) {
-            Toast.fire({
-              icon: 'warning',
-              title: '請重新操作',
-            });
-          }
-          // end of !res?.status
-
-          if (res?.status) {
-            Toast.fire({
-              icon: 'success',
-              title: '註冊成功',
-              didClose: () => {
-                setTimeout(() => {
-                  navigate('/login');
-                }, 400);
-              },
-            });
-          }
-          // end of res?.status
-
-          resetForm();
+    api.signup(sendData).then((res: any) => {
+      if (!res?.status) {
+        Toast.fire({
+          icon: 'warning',
+          title: '請重新操作',
         });
-        // end of api
-      }}
+      }
+      // end of !res?.status
+
+      if (res?.status) {
+        Toast.fire({
+          icon: 'success',
+          title: '註冊成功',
+          didClose: () => {
+            setTimeout(() => {
+              navigate('/login');
+            }, 400);
+          },
+        });
+      }
+      // end of res?.status
+
+      reset();
+    });
+    // // end of api
+  };
+
+  return (
+    <form
+      className="text-center"
+      id="swal-target"
+      onSubmit={handleSubmit(onSubmitHandler)}
     >
-      {({ errors, touched, isValid, dirty }) => (
-        <Form className="text-center" id="swal-target">
-          <h2 className="mb-6 text-2xl font-bold">註冊帳號</h2>
+      <h2 className="mb-6 text-2xl font-bold">註冊帳號</h2>
 
-          <div className="mb-4 text-left">
-            <label htmlFor="email" className="text-sm font-bold">
-              <p className="mb-1">Email</p>
+      <div className="mb-4 text-left">
+        <label htmlFor="email" className="text-sm font-bold">
+          <p className="mb-1">Email</p>
 
-              <Field
-                type="email"
-                name="email"
-                placeholder="請輸入 Email"
-                className="w-full rounded-[10px] px-4 py-3 font-medium placeholder:text-light"
-              />
-            </label>
+          <input
+            type="email"
+            placeholder="請輸入 Email"
+            className="w-full rounded-[10px] px-4 py-3 font-medium placeholder:text-light"
+            {...register('email')}
+            required
+          />
+        </label>
 
-            {errors.email && touched.email ? <div>{errors.email}</div> : null}
-          </div>
+        <p>{errors.email?.message}</p>
+      </div>
 
-          <div className="mb-4 text-left">
-            <label htmlFor="nickname" className="text-sm font-bold">
-              <p className="mb-1">您的暱稱</p>
+      <div className="mb-4 text-left">
+        <label htmlFor="nickname" className="text-sm font-bold">
+          <p className="mb-1">您的暱稱</p>
 
-              <Field
-                type="text"
-                name="nickname"
-                placeholder="請輸入暱稱"
-                className="w-full rounded-[10px] px-4 py-3 font-medium placeholder:text-light"
-              />
-            </label>
+          <input
+            type="text"
+            placeholder="請輸入暱稱"
+            className="w-full rounded-[10px] px-4 py-3 font-medium placeholder:text-light"
+            {...register('nickname')}
+            required
+          />
+        </label>
 
-            {!!errors.nickname && (
-              <p className="text-error">{errors.nickname}</p>
-            )}
-          </div>
+        <p>{errors.nickname?.message}</p>
+      </div>
 
-          <div className="mb-4 text-left">
-            <label htmlFor="password" className="text-sm font-bold">
-              <p className="mb-1">密碼</p>
+      <div className="mb-4 text-left">
+        <label htmlFor="password" className="text-sm font-bold">
+          <p className="mb-1">密碼</p>
 
-              <Field
-                type="password"
-                name="password"
-                placeholder="請輸入密碼"
-                className="w-full rounded-[10px] px-4 py-3 font-medium placeholder:text-light"
-              />
-            </label>
+          <input
+            type="password"
+            placeholder="請輸入密碼"
+            className="w-full rounded-[10px] px-4 py-3 font-medium placeholder:text-light"
+            {...register('password')}
+            required
+          />
+        </label>
 
-            {errors.password && touched.password && errors.password}
-          </div>
+        <p>{errors.password?.message}</p>
+      </div>
 
-          <div className="mb-3 text-left">
-            <label htmlFor="confirm-password" className="text-sm font-bold">
-              <p className="mb-1">再次輸入密碼</p>
+      <div className="mb-3 text-left">
+        <label htmlFor="confirm-password" className="text-sm font-bold">
+          <p className="mb-1">再次輸入密碼</p>
 
-              <Field
-                type="password"
-                name="confirmPassword"
-                placeholder="請再次輸入密碼"
-                className="w-full rounded-[10px] px-4 py-3 font-medium placeholder:text-light"
-              />
-            </label>
+          <input
+            type="password"
+            placeholder="請再次輸入密碼"
+            className="w-full rounded-[10px] px-4 py-3 font-medium placeholder:text-light"
+            {...register('confirmPassword')}
+            required
+          />
+        </label>
 
-            {!!errors.confirmPassword && (
-              <p className="text-error">{errors.confirmPassword}</p>
-            )}
-          </div>
+        <p>{errors.confirmPassword?.message}</p>
+      </div>
 
-          <button
-            type="submit"
-            className={`mb-6 rounded-[10px] px-12 py-3 font-bold text-white ${
-              isValid && dirty
-                ? 'cursor-pointer bg-dark'
-                : 'cursor-not-allowed bg-light'
-            }`}
-          >
-            註冊帳號
-          </button>
+      <button
+        type="submit"
+        className={`mb-6 rounded-[10px] px-12 py-3 font-bold text-white ${
+          !errors.confirmPassword?.message &&
+          watch('nickname') &&
+          watch('email') &&
+          watch('password') &&
+          watch('confirmPassword')
+            ? 'cursor-pointer bg-dark'
+            : 'cursor-not-allowed bg-light'
+        }`}
+      >
+        註冊帳號
+      </button>
 
-          <Link to="/login" className="block font-bold">
-            登入
-          </Link>
-        </Form>
-      )}
-    </Formik>
+      <Link to="/login" className="block font-bold">
+        登入
+      </Link>
+    </form>
   );
 }
 
